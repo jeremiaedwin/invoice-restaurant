206ee08204c611a9ed78cea10778dd44d8734c4f
jeremiaedwin <edwinjeremia00@gmail.com>
2021-12-08 16:43:25 +0700

    login page

---
+++ login.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void viewLogin(char[20] *username, char[10] *password);
+
+int main()
+{
+	char username[20];
+	char password[10];
+	
+	return 0;
+}
+
+void viewLogin(char[20] *username, char[10] *password){
+	
+}
